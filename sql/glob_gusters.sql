4843dd5015a7331e43ed15571f688cbfafec0edb
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2026-09-21 17:04:53 -0500

    feat: Agregar tablas Nacionalidad y Actor en glob_gusters.sql

--- sql/glob_gusters.sql
+++ sql/glob_gusters.sql
@@ -8,3 +8,18 @@ USE `glob_gusters`;
 
 -- Eliminación de la base de datos
 -- DROP DATABASE IF EXISTS `glob_gusters`;
+
+-- DROP TABLE IF EXISTS Persons;
+
+CREATE TABLE IF NOT EXISTS Nacionalidad (
+    NacionalidadID int PRIMARY KEY,
+    Nombre varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Actor (
+    ActorID int PRIMARY KEY,
+    Nombre varchar(100) NOT NULL,
+    Sexo varchar(20) NOT NULL,
+    NacionalidadID int,
+    FOREIGN KEY (NacionalidadID) REFERENCES Nacionalidad(NacionalidadID)
+);
